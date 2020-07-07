dd3e4cff85f5186ed0bb44aa17c62c29d45bb0ba
GitNAG0 <fladnatas@gmail.com>
2020-07-06 18:48:09 -0700

    navbar working?

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,7 +4,7 @@ import './style.css';
 
 function Index() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <Link className="navbar-brand" to="/">
           GoggleBuks
         </Link>
